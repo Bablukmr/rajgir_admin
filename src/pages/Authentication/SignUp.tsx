52e52f91274238b3260c15793227dfe83b48f912
Bablukmr <bablukmr800@gmail.com>
2024-11-19 13:41:59 +0530

    Sign Up to Admin

--- src/pages/Authentication/SignUp.tsx
+++ src/pages/Authentication/SignUp.tsx
@@ -19,8 +19,7 @@ const SignUp: React.FC = () => {
             <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
               {/* <span className="mb-1.5 block font-medium">Start for free</span> */}
               <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
-                Sign Up to Admin
-              </h2>
+                Create New User              </h2>
 
               <form>
                 <div className="mb-4">
